99ccbf49e639c8f98536c04763eedcdbbb74bbd9
BrendaDenhoff <brenda.denhoff@estudiantes.unahur.edu.ar>
2022-11-05 22:49:13 -0300

    Subida inicial

---
+++ CoderHouse-C-Sharp-Denhoff/Venta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public string Comentarios { get; set; }
+
+        public Venta(int id, string comentarios)
+        {
+            this.Id = id;
+            this.Comentarios = comentarios;
+        }
+    }
+}
